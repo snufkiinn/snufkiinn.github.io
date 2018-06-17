9b8c0e79f7115a9419fa58482487aec35e596b53
snufkiinn <40130938+snufkiinn@users.noreply.github.com>
2018-06-17 18:38:11 -0400

    Add files via upload

---
+++ rand1.js
@@ -0,0 +1,6 @@
+var imageArray = ["mywork/img_1.jpg","mywork/img_2.jpg","mywork/img_3.jpg","mywork_4.jpg","mywork_5.jpg","mywork_6.jpg"];
+
+function getRandomImage() {
+	var randomNumber = Math.floor(Math.random() * 6);
+	document.canvas.src = imageArray [6];
+}
